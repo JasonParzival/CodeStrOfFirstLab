ba35c44e226b013d6c4ebe593a8ed457e7537e79
JasonParzival <ruslanmuhamed123@gmail.com>
2025-02-10 23:01:31 +0800

    Теперь полностью работает

--- TheThirdCode/Program.cs
+++ TheThirdCode/Program.cs
@@ -4,17 +4,28 @@
     {
         static void Main(string[] args)
         {
-            string str = "I wanna eat guobaorou with rice and di san xian";
-            string dubl = "";
+            string str = "I wanna eat guobaorou with rice and di san xian dad";
             string[] words = str.Split();
+            string needword = "";
 
             foreach (string word in words)
             {
                 if(word.Length % 2 == 1) //если нечетное число букв
                 {
-                    Console.Write(word + " " + word + " ");
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word[i] == 'd')
+                        {
+                            needword += "t";
+                        }
+                        else
+                        {
+                            needword += word[i];
+                        }
+                    }
+                    Console.Write(needword + " " + needword + " ");
+                    needword = "";
                 }
-                //Console.WriteLine(word);
             }
 
         }
