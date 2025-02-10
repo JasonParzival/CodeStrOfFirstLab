920ad36d820ee3cf9511333a069e6b3650ed9b64
JasonParzival <ruslanmuhamed123@gmail.com>
2025-02-10 22:51:59 +0800

    Добавьте файлы проекта.

---
+++ TheThirdCode/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace TheThirdCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "I wanna eat guobaorou with rice and di san xian";
+            string dubl = "";
+            string[] words = str.Split();
+
+            foreach (string word in words)
+            {
+                if(word.Length % 2 == 1) //если нечетное число букв
+                {
+                    Console.Write(word + " " + word + " ");
+                }
+                //Console.WriteLine(word);
+            }
+
+        }
+    }
+}
