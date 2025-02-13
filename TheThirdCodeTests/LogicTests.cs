e76cd4b64259aba119ee0b607e804e57c0de6c0b
JasonParzival <ruslanmuhamed123@gmail.com>
2025-02-13 21:14:43 +0800

    Добавил тесты

---
+++ TheThirdCodeTests/LogicTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheThirdCode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheThirdCode.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void DefaultTest()
+        {
+            string str = "I wanna eat guobaorou with rice and di san xian dad";
+
+            string message = Logic.Message(str);
+
+            Assert.AreEqual("I I wanna wanna eat eat guobaorou guobaorou ant ant san san tat tat ", message);
+        }
+
+        [TestMethod()]
+        public void DeleteTextTest()
+        {
+            string str = "Huh at Gor at";
+
+            string message = Logic.Message(str);
+
+            Assert.AreEqual("Huh Huh Gor Gor ", message);
+        }
+
+        [TestMethod()]
+        public void ReplaceLettersTest()
+        {
+            string str = "dough downe Irkudsk";
+
+            string message = Logic.Message(str);
+
+            Assert.AreEqual("tough tough towne towne Irkutsk Irkutsk ", message);
+        }
+    }
+}
